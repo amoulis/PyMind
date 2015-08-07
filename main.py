5f623ba896ae98bd62fce46254af44e045460a11
amoulis <adrien.moulis@gmail.com>
2015-08-07 15:31:36 +0200

    generated random sequence ok

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 - 8 colors, here replaced by numbers from 1 to 7.
 - The player needs to guess a 4 color combinaison (that would be generated randomly)
 - After each trial the player knows the number
+Mastermind was invented by Mordecai Meirowitz in 1970
 
 PyMind works with was developped and tested with Python3.4
 """
 
 import sys
 import os
+import random
 
 # clear console
 clear = lambda: os.system('cls')
@@ -17,9 +19,18 @@ clear()
 print("\n \n \t Welcome to PyMind")
 print("\t Hope you'll enjoy the game")
 
-# generate the random combinaison
+nbTrials = 10
+
+# generate random combinaison
+random.seed();
+list = []
+for x in range(0, 4):
+	list.append(random.randint(1,7))
+#print(list)
 
 # start loop
+while(nbTrials > 0):
+	print("in loop")
 
 # analyze player's n-th guess
 
