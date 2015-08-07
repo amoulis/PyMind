617ece2609ec916291c2e60642051a21d2ce7ea2
amoulis <adrien.moulis@gmail.com>
2015-08-07 15:16:32 +0200

    added main file

---
+++ main.py
@@ -0,0 +1,26 @@
+""" PyMind is inspired by the 'mastermind' game.
+- 8 colors, here replaced by numbers from 1 to 7.
+- The player needs to guess a 4 color combinaison (that would be generated randomly)
+- After each trial the player knows the number
+
+PyMind works with was developped and tested with Python3.4
+"""
+
+import sys
+import os
+
+# clear console
+clear = lambda: os.system('cls')
+clear()
+
+# print friendly message to user
+print("\n \n \t Welcome to PyMind")
+print("\t Hope you'll enjoy the game")
+
+# generate the random combinaison
+
+# start loop
+
+# analyze player's n-th guess
+
+# assert results (number of color well placed, misplaced)
